9c7f3ee9570947a8644f2e229026e3b979109a67
victorfrei <vic.frei2@gmail.com>
2022-11-13 16:26:38 -0300

    Fix: Remoção logo principal

--- components/HeaderMainPage.tsx
+++ components/HeaderMainPage.tsx
@@ -50,7 +50,7 @@ export default function HeaderMainPage({ nav }: HeaderMainPageProps) {
         <nav className="fixed w-full bg-white text-center border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
             <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                 <a href="https://flowbite.com" className="flex items-center">
-                    <Image width={50} height={50} src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                    {/* <Image width={50} height={50} src="https://flowbite.com/docs/images/logo.svg" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" /> */}
                     <span className="self-center text-xl font-bold whitespace-nowrap dark:text-white">Coworking</span>
                 </a>
 
